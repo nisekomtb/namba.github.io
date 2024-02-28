f421f37b1c91ef250685dc84416969406ba1649b
Tom Mortiboy <tom.mortiboy@gmail.com>
2024-02-28 14:55:15 +0900

    Twin Peaks WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
-/** 
+/**
  *------------------------------------------------------------------------------
  * @package       T3 Framework for Joomla!
  *------------------------------------------------------------------------------
  * @copyright     Copyright (C) 2004-2013 JoomlArt.com. All Rights Reserved.
  * @license       GNU General Public License version 2 or later; see LICENSE.txt
- * @authors       JoomlArt, JoomlaBamboo, (contribute to this project at github 
+ * @authors       JoomlArt, JoomlaBamboo, (contribute to this project at github
  *                & Google group to become co-author)
  * @Google group: https://groups.google.com/forum/#!forum/t3fw
- * @Link:         http://t3-framework.org 
+ * @Link:         http://t3-framework.org
  *------------------------------------------------------------------------------
  */
 
- (function($){
-  $(document).ready(function(){
+(function ($) {
+  $(document).ready(function () {
 
     ////////////////////////////////
     // equalheight for col
@@ -20,42 +20,42 @@
     var ehArray = ehArray2 = [],
       i = 0;
 
-    $('.equal-height').each (function(){
+    $('.equal-height').each(function () {
       var $ehc = $(this);
-      if ($ehc.has ('.equal-height')) {
+      if ($ehc.has('.equal-height')) {
         ehArray2[ehArray2.length] = $ehc;
       } else {
         ehArray[ehArray.length] = $ehc;
       }
     });
 
-    for (i = ehArray2.length -1; i >= 0; i--) {
+    for (i = ehArray2.length - 1; i >= 0; i--) {
       ehArray[ehArray.length] = ehArray2[i];
     }
 
-    var equalHeight = function() {
+    var equalHeight = function () {
       for (i = 0; i < ehArray.length; i++) {
         var $cols = ehArray[i].children().filter('.col'),
           maxHeight = 0,
           equalChildHeight = ehArray[i].hasClass('equal-height-child');
 
-      // reset min-height
+        // reset min-height
         if (equalChildHeight) {
-          $cols.each(function(){$(this).children().first().css('min-height', 0)});
+          $cols.each(function () { $(this).children().first().css('min-height', 0) });
         } else {
           $cols.css('min-height', 0);
         }
-        $cols.each (function() {
+        $cols.each(function () {
           maxHeight = Math.max(maxHeight, equalChildHeight ? $(this).children().first().innerHeight() : $(this).innerHeight());
         });
         if (equalChildHeight) {
-          $cols.each(function(){$(this).children().first().css('min-height', maxHeight + 1)});
+          $cols.each(function () { $(this).children().first().css('min-height', maxHeight + 1) });
         } else {
           $cols.css('min-height', maxHeight + 1);
         }
       }
       // store current size
-      $('.equal-height > .col').each (function(){
+      $('.equal-height > .col').each(function () {
         var $col = $(this);
         $col.data('old-width', $col.width()).data('old-height', $col.innerHeight());
       });
@@ -64,11 +64,11 @@
     equalHeight();
 
     // monitor col width and fire equalHeight
-    setInterval(function() {
-      $('.equal-height > .col').each(function(){
+    setInterval(function () {
+      $('.equal-height > .col').each(function () {
         var $col = $(this);
         if (($col.data('old-width') && $col.data('old-width') != $col.width()) ||
-            ($col.data('old-height') && $col.data('old-height') != $col.innerHeight())) {
+          ($col.data('old-height') && $col.data('old-height') != $col.innerHeight())) {
           equalHeight();
           // break each loop
           return false;
@@ -79,8 +79,8 @@
 
   // Add Inview
   $(document).ready(function () {
-    if($('.enable-effect').length > 0) {
-      $('.t3-section-wrap > div,.t3-hero').bind('inview', function(event, visible) {
+    if ($('.enable-effect').length > 0) {
+      $('.t3-section-wrap > div,.t3-hero').bind('inview', function (event, visible) {
         if (visible) {
           $(this).addClass('ja-inview');
         }
